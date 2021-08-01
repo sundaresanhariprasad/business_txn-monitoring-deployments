165712054f292be3ae1934aa3bdc2b58e5c2b75d
sundaresanhariprasad <hari.gcp.study@gmail.com>
2021-08-01 03:03:18 +0100

    initial version

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "setup starts"
-gcloud container clusters get-credentials elk-btm --zone europe-west1-c --project hariprasad-sundaresan-308010
+gcloud container clusters get-credentials business-transaction-monitoring --zone europe-west1-c --project hariprasad-sundaresan-308010
 helm repo add elastic https://helm.elastic.co
 helm repo update
-helm install btm-chart ./btm-charts
+helm business-transaction-monitoring ./btm-charts
 echo "setup ends"
